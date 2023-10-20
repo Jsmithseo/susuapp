0715ea8addafb33ee444eb46c6279e34d4163e1b
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-10-19 21:24:10 -0700

    wip

--- pages/earlyaccess/index.js
+++ pages/earlyaccess/index.js
@@ -36,7 +36,7 @@ export default function EarlyAccess() {
               feedback. Your input directly influences the final product.
             </li>
             <li>
-              <b>First Look:</b>Get a head start on the app's features and
+              <b>First Look:</b> Get a head start on the app's features and
               capabilities. You'll be among the select few to explore what it
               has to offer before it's released to the general public.
             </li>
@@ -46,7 +46,7 @@ export default function EarlyAccess() {
               lead to improvements and enhancements based on real-world usage.
             </li>
             <li>
-              <b>Exclusive Updates:</b>Enjoy early updates, bug fixes, and new
+              <b>Exclusive Updates: </b> Enjoy early updates, bug fixes, and new
               features as they're rolled out during the beta phase. You won't
               have to wait for the official release to benefit from
               improvements.
@@ -58,7 +58,7 @@ export default function EarlyAccess() {
             </li>
           </ol>
 
-          <h3>How to Get Early Access:</h3>
+          <h3>How to Get Early Access: </h3>
           <p>
           We'll send you an email to help you get started on your journey to early access. To become an early access user and unlock the future of our app, simply complete the provided form. We'll walk you through the process, allowing you to dive into the app's potential and actively contribute to its development.
           </p>

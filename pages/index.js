665685388c8f4f0fb8a8c75583b9688778352e4b
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-10-19 17:08:04 -0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -151,8 +151,8 @@ export default function Index() {
       <Col
         md="12"
         lg="12"
-        sm="8"
-        xs="8"
+        sm="9"
+        xs="9"
         className="power-text d-flex justify-content-end"
       >
         <b> Powered By: &nbsp; </b>{' '}
